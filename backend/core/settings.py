791f5464639fe1f591e8cb35cc4b7b11397a109b
Pranjal Shrvastava <pshriv365@gmail.com>
2026-04-02 01:31:21 -0700

    render env modification

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-up4rgwzwxi-wua0svi73+b5^(fvqz+@+4^e@pdxo_0e)kd&w4k'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-up4rgwzwxi-wua0svi73+b5^(fvqz+@+4^e@pdxo_0e)kd&w4k",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").strip().lower() == "true"
